894b87d8b8d900f380afe3f964132aeac39f2781
Rishab Goyal <rishabgoyal432@gmail.com>
2021-04-09 02:01:34 +0530

    Axios Async get request for contests

--- frontend/src/Pages/ContestList/ContestPage.jsx
+++ frontend/src/Pages/ContestList/ContestPage.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Nav, NavItem, TabContent, TabPane, NavLink, Col, Row } from 'reactstrap';
-import { Link } from "react-router-dom";
+import axios from 'axios';
 import HeaderComponent from '../../components/HeaderComponent';
 import FooterComponent from '../../components/FooterComponent';
 const CONTESTS =
@@ -8,15 +8,15 @@ const CONTESTS =
     {
       contest_id: 'C1',
       contest_name: 'Contest 1(Upcomming)',
-      start_time: '2021-04-05 15:00:00',
-      end_time: '2021-04-05 17:00:00',
+      start_time: '2021-05-05 15:00:00',
+      end_time: '2021-06-05 17:00:00',
       contest_img: '/assets/img/2842680.jpg'
     },
     {
       contest_id: 'C2',
       contest_name: 'Contest 2(live)',
       start_time: '2021-03-25 15:00:00',
-      end_time: '2021-04-05 17:00:00',
+      end_time: '2022-04-05 17:00:00',
       contest_img: '/assets/img/logo.jpeg'
     },
     {
@@ -43,13 +43,39 @@ function RenderCards({ contest }) {
     </div>
   );
 }
-const Contests = (props) => {
-  const [activeTab, setActiveTab] = useState('1');
+class Contests extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: '1',
+      contests:[]
+    }
+    this.toggleActiveTab = this.toggleActiveTab.bind(this);
+  }
 
-  const toggle = tab => { if (activeTab !== tab) setActiveTab(tab); }
+  componentDidMount() {
+    this.getContests();
+  }
+  // Axios Async request for contest data
+  getContests = async () => {
+    try {
+      const res = await axios.get(`http://104.211.91.225:5000/login`);
+      this.setState({contests: res.data});
+    } catch (err) {
+      console.log(err);
+      this.setState({ contests:[]});
+    }
+  };
 
+  toggleActiveTab = (tab) => {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab,
+      });
+    }
+  }
   // Rendering Live contests
-  const live = CONTESTS.filter((contest) =>
+  live = CONTESTS.filter((contest) =>
     ((new Date(contest.start_time)).getTime() < (new Date()).getTime()) && ((new Date(contest.end_time)).getTime() > (new Date()).getTime())
   ).map((contest) => {
     return (
@@ -60,7 +86,7 @@ const Contests = (props) => {
   });
 
   // Rendering Upcoming contests
-  const upcomming = CONTESTS.filter((contest) =>
+  upcomming = CONTESTS.filter((contest) =>
     (new Date(contest.start_time)).getTime() > (new Date()).getTime()
   ).map((contest) => {
     return (
@@ -71,7 +97,7 @@ const Contests = (props) => {
   });
 
   // Rendering previous contests
-  const previous = CONTESTS.filter((contest) =>
+  previous = CONTESTS.filter((contest) =>
     (new Date(contest.end_time)).getTime() < (new Date()).getTime()
   ).map((contest) => {
     return (
@@ -80,92 +106,66 @@ const Contests = (props) => {
       </div>
     );
   });
-
-  return (
-    <>
-      <HeaderComponent />
-      
-      <div className="jumbotron" style={{ height: "300px" }}>
+  render() {
+    return (
+      <>
+        <HeaderComponent />
+        <div className="jumbotron" style={{ height: "300px" }}>
+          <div className="container">
+            <h1>Contests & <br /> Programming Challenges</h1>
+          </div>
+        </div>
         <div className="container">
-          <h1>Contests & <br /> Programming Challenges</h1>
+          <br />
+          <div >
+            <div className="d-flex justify-content-center">
+              <Nav tabs className="row w-100 d-flex align-items-center justify-content-center">
+                <NavItem className="col-4 text-center px-0">
+                  <NavLink
+                    className={this.state.activeTab == '1' ? 'active' : ''}
+                    onClick={() => { this.toggleActiveTab('1'); }}>
+                    LIVE
+              </NavLink>
+                </NavItem>
+                <NavItem className="col-4 text-center px-0">
+                  <NavLink
+                    className={this.state.activeTab == '2' ? 'active' : ''}
+                    onClick={() => { this.toggleActiveTab('2'); }}>
+                    UPCOMMING
+              </NavLink>
+                </NavItem>
+                <NavItem className="col-4 text-center px-0">
+                  <NavLink
+                    className={this.state.activeTab == '3' ? 'active' : ''}
+                    onClick={() => { this.toggleActiveTab('3'); }}>
+                    PREVIOUS
+              </NavLink>
+                </NavItem>
+              </Nav>
+            </div>
+            <TabContent activeTab={this.state.activeTab}>
+              <TabPane tabId="1">
+                <Row>
+                  {this.live}
+                </Row>
+              </TabPane>
+              <TabPane tabId="2">
+                <Row>
+                  {this.upcomming}
+                </Row>
+              </TabPane>
+              <TabPane tabId="3">
+                <Row>
+                  {this.previous}
+                </Row>
+              </TabPane>
+            </TabContent>
+          </div>
         </div>
-      </div>
-      <div className="container">
-        <br />
-        <div>
-          <Nav tabs className="row w-100 d-flex align-items-center justify-content-center">
-            <NavItem className="col-4 text-center px-0">
-              <NavLink
-                className={activeTab == '1' ? 'active' : ''}
-                onClick={() => { toggle('1'); }}>
-                LIVE
-              </NavLink>
-            </NavItem>
-            <NavItem className="col-4 text-center px-0">
-              <NavLink
-                className={activeTab == '2' ? 'active' : ''}
-                onClick={() => { toggle('2'); }}>
-                UPCOMMING
-              </NavLink>
-            </NavItem>
-            <NavItem className="col-4 text-center px-0">
-              <NavLink
-                className={activeTab == '3' ? 'active' : ''}
-                onClick={() => { toggle('3'); }}>
-                PREVIOUS
-              </NavLink>
-            </NavItem>
-          </Nav>
-          <TabContent activeTab={activeTab}>
-            <TabPane tabId="1">
-              <Row>
-                {live}
-              </Row>
-            </TabPane>
-            <TabPane tabId="2">
-              <Row>
-                {upcomming}
-              </Row>
-            </TabPane>
-            <TabPane tabId="3">
-              <Row>
-                {previous}
-              </Row>
-            </TabPane>
-          </TabContent>
-        </div>
-        {/* <Nav tabs>
-            <NavItem className="">
-              <Link className={this.state.activeTab == '1' ? 'active' : ''} onClick={() => this.setActiveTab('1')} style={{ cursor: "pointer" }}>
-                <h5 className="">UPCOMMING</h5>
-              </Link>
-            </NavItem>
-            <NavItem className="">
-              <Link className={this.state.activeTab == '2' ? 'active' : ''} onClick={() => this.setActiveTab('2')} style={{ cursor: "pointer" }}>
-                <h5 className="">LIVE</h5>
-              </Link>
-            </NavItem>
-            <NavItem className="">
-              <Link className={this.state.activeTab == '3' ? 'active' : ''} onClick={() => this.setActiveTab('3')} style={{ cursor: "pointer" }}>
-                <h5 className="">PREVIOUS</h5>
-              </Link>
-            </NavItem>
-          </Nav>
-          <TabContent activeTab={this.state.activeTab}>
-            <TabPane tabId="1">
-              Upcomming
-            </TabPane>
-            <TabPane tabId="2">
-              live
-            </TabPane>
-            <TabPane tabId="3">
-              previous
-            </TabPane>
-          </TabContent> */}
-      </div>
-      <FooterComponent />
-    </>
-  );
+        <FooterComponent />
+      </>
+    );
+  }
 }
 
 export default Contests;
